2d15967379fdb0327cfb53529f6255abba21028f
Ramakant-97 <166520409+Ramakant-97@users.noreply.github.com>
2024-04-12 06:03:29 +0000

    container confg done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "Bobbysengar17@gamail.com"
+var accountpassword = "12345"
+accountcity="Indore"
+let accountState;
+
+accountEmail ="hbc@goggle.com"
+accountpassword = "123434"
+accountcity = "Benguluru"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as new version
+//alert(3+3)//we are using node.js not browser
+
+console.log(3
+    +3)//code should be readale
+
+
+
+
+console.log(typeof "bobby")
+console.log(typeof null)
+console.log(typeof undefined)
