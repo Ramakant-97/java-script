a337cf71f4815c6c3fd36b0ae54763e736efa2ac
Ramakant-97 <166520409+Ramakant-97@users.noreply.github.com>
2024-05-17 04:11:34 +0000

    converstion of datatypes

--- 01_basic/03_converstionoperation.js
+++ 01_basic/03_converstionoperation.js
@@ -14,3 +14,12 @@ console.log( valueInNumber);
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
+
+//""=>false
+//"Ramakant"=>True
+//1=>True; 0=>false
+
+let SomeNumber=77
+let stringSomeNumber=String(SomeNumber)
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
