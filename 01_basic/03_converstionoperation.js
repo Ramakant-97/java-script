9be119ccf2ba05dc80fb7ccfb7c63da65155d373
Ramakant-97 <166520409+Ramakant-97@users.noreply.github.com>
2024-05-17 07:57:24 +0000

    comparsion between datatypes

--- 01_basic/03_converstionoperation.js
+++ 01_basic/03_converstionoperation.js
@@ -23,3 +23,24 @@ let SomeNumber=77
 let stringSomeNumber=String(SomeNumber)
 console.log(stringSomeNumber)
 console.log(typeof stringSomeNumber)
+
+
+/***********************************operations***************************************/
+
+
+let value = 99
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "Ramakant"
+let str2 = "Sengar"
+let str3= str1 + str2
+console.log(str3)
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+3+6);
+console.log(1+5+4+"8");
+console.log((3+5)*8%5)
+
