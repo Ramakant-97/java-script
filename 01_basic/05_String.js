69acb3bbf0446361a0848b4c1365946c08f0857f
Ramakant-97 <166520409+Ramakant-97@users.noreply.github.com>
2024-05-21 04:08:19 +0000

    String

---
+++ 01_basic/05_String.js
@@ -0,0 +1,24 @@
+const name ="Bobby"
+const repocount =50
+//console.log(  name + repocount + "Value")
+
+console.log( `Hello My name is ${name} and my repocount ${repocount}`)
+
+const gameName = new String('deva')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
+const newString = gameName.substring(0,3)
+console.log(newString)
+
+const anotherstring = gameName.slice(-4,1)
+console.log(anotherstring );
+
+const newStringOne = "   hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
